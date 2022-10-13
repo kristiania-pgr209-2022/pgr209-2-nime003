d4a3f29247ec5172867dce6d486bd5cf08dae22b
nime003 <nime003@student.kristiania.no>
2022-10-13 18:14:40 +0200

    ❤httpResponseShouldBe200Test (compiles, but server is null) *added jetty

--- src/main/java/no/kristiania/nimebu/ShopServer.java
+++ src/main/java/no/kristiania/nimebu/ShopServer.java
@@ -1,7 +1,27 @@
 package no.kristiania.nimebu;
 
+import org.eclipse.jetty.server.Server;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class ShopServer {
+
+    private final Server shopServer;
+
+    public ShopServer(int port) {
+        this.shopServer = new Server(port);
+    }
+
+    public void start() throws Exception {
+        shopServer.start();
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    public URL getURL() throws MalformedURLException {
+        return shopServer.getURI().toURL();
     }
 }
--- src/test/java/no/kristiania/nimebu/ShopServerTest.java
+++ src/test/java/no/kristiania/nimebu/ShopServerTest.java
@@ -11,11 +11,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShopServerTest {
-
+    public static ShopServer server;
     @BeforeEach
-    void setup(){
-        var server = new ShopServer(0);
-        server.start;
+    void setup() throws Exception {
+        server = new ShopServer(0);
+        server.start();
     }
 
     @Test
