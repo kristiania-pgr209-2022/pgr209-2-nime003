ee52e84cb0ee47661924fb7d64a6ec83b572059a
nime003 <nime003@student.kristiania.no>
2022-10-13 17:30:21 +0200

    Initial setup commit.

---
+++ src/main/java/no/kristiania/nimebu/ShopServer.java
@@ -0,0 +1,7 @@
+package no.kristiania.nimebu;
+
+public class ShopServer {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/no/kristiania/nimebu/ShopServerTest.java
@@ -0,0 +1,7 @@
+package no.kristiania.nimebu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopServerTest {
+
+}
