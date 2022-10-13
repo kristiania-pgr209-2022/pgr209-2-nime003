5a32a23a9fa342d40ffb4be82b15de1499d7818c
nime003 <nime003@student.kristiania.no>
2022-10-13 18:31:49 +0200

    ❤shouldServeH1Test

--- src/test/java/no/kristiania/nimebu/ShopServerTest.java
+++ src/test/java/no/kristiania/nimebu/ShopServerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,6 +23,12 @@ class ShopServerTest {
     void httpResponseShouldBe200Test() throws IOException {
         var connection = openConnection("/");
         assertThat(connection.getResponseCode()).isEqualTo(200);
+    }
+
+    @Test
+    public void shouldServeH1Test() throws IOException {
+        var connection = openConnection("/");
+        assertThat(connection.getInputStream()).asString(StandardCharsets.UTF_8).contains("<h1>nimebu Shop!</h1>");
     }
 
     private HttpURLConnection openConnection(String spec) throws IOException {
