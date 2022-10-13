4225cdb2acc94a508283e8ff2d22ac5a9d05b8be
“Mebu98” <merijn.buitelaar98@gmail.com>
2022-10-13 17:46:20 +0200

    ❤❤❤httpResponseShouldBe200Test (doesn't even compile)
    *added assertj-core
    *added junit

--- src/test/java/no/kristiania/nimebu/ShopServerTest.java
+++ src/test/java/no/kristiania/nimebu/ShopServerTest.java
@@ -1,7 +1,30 @@
 package no.kristiania.nimebu;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShopServerTest {
 
+    @BeforeEach
+    void setup(){
+        var server = new ShopServer(0);
+        server.start;
+    }
+
+    @Test
+    void httpResponseShouldBe200Test() throws IOException {
+        var connection = openConnection("/");
+        assertThat(connection.getResponseCode()).isEqualTo("200");
+    }
+
+    private HttpURLConnection openConnection(String spec) throws IOException {
+        return (HttpURLConnection) new URL(server.getURL(), spec).openConnection();
+    }
 }
