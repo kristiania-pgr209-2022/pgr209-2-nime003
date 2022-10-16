dad87f45a0faa085348c61e1f5da73adfb637b56
“Mebu98” <merijn.buitelaar98@gmail.com>
2022-10-16 19:05:31 +0200

    List all products with React

--- src/main/nimebushop-frontend/src/App.jsx
+++ src/main/nimebushop-frontend/src/App.jsx
@@ -1,12 +1,36 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+
+function AddProduct() {
+  return null;
+}
+
+function ListProducts() {
+    const [loading, setLoading] = useState(true);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const res = await fetch("api/products");
+            setProducts(await res.json());
+            setLoading(false);
+        })();
+    }, [])
+
+  if (loading){
+      return <div>Loading products ...</div>
+  }
+  return <ul>{products.map(p => <div>produkt id {p.productId} : {p.productName}</div>)}</ul>
+}
 
 function App() {
 
   return (
     <div className="App">
-
+        <h1>Welcome to Nimebu Shop!</h1>
+        <AddProduct/>
+        <ListProducts/>
     </div>
   )
 }
